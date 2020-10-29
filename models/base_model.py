3fe93aab5af29c6068144fc6b2fdbf4aa0212411
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-10-29 17:39:59 -0500

    update branch

--- models/base_model.py
+++ models/base_model.py
@@ -2,27 +2,78 @@
 """BaseModel module
 """
 import uuid
+from datetime import datetime
 
 
 class BaseModel:
-    """Base
-
-    Attrs:
-        nb_objects (int): private attribute that
-        indicates the number of objects.
-
     """
-    __nb_objects = 0
-
-    def __init__(self, id=None):
+    BaseModel constructor.
+    """
+    def __init__(self, *args, **kwargs):
         """Init method.
 
         Args:
-            id (str):  public instance attribute that identifies the object.
+            kwargs:
+            id: (string) - assign with an uuid when an instance is created:
+            you can use uuid.uuid4() to generate unique id but don’t forget
+            to convert to a string. The goal is to have unique id for each
+            BaseModel.
+            created_at: (datetime) - assign with the current datetime when an
+            instance is created.
+            updated_at: (datetime) - assign with the current datetime when an
+            instance is created and it will be updated every time you change
+            your object.
 
         """
-        if id is None:
-            BaseModel.__nb_objects += 1
-            self.id = BaseModel.__nb_objects
+        format = '%Y-%m-%dT%H:%M:%S.%f'
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at':
+                    self.created_at = datetime.strptime(value, format)
+                if key == 'updated_at':
+                    self.updated_at = datetime.strptime(value, format)
+                else:
+                    if key != __class__:
+                        setattr(self, key, value)
         else:
-            self.id = id
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """repr method.
+
+        Args:
+            None
+
+        """
+        representation = "[%s] (%s) %s" % (self.__class__.__name__,
+                                           self.id,
+                                           self.__dict__)
+        return(representation)
+
+    def save(self):
+        """ Public method that updates the public instance
+        attribute updated_at with the current datetime.
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of
+        __dict__ of the instance:
+
+        by using self.__dict__, only instance attributes set will be returned
+        a key __class__ must be added to this dictionary with the class name
+        of the object.
+        created_at and updated_at must be converted to string object in ISO
+        format: %Y-%m-%dT%H:%M:%S.%f (ex: 2017-06-14T22:31:03.285259).
+        This method is the first piece of the serialization/deserialization
+        process: create a dictionary representation with “simple object type”
+        of the BaseModel.
+
+        """
+        new_dict = dict(self.__dict__)
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        new_dict['__class__'] = self.__class__.__name__
+        return (new_dict)
