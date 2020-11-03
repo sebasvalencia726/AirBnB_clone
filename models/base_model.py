ea68df40dc21c473436d383bdcb2b60fed11f6f4
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-11-02 19:55:36 -0500

    Base_model update according to file_storage changes

--- models/base_model.py
+++ models/base_model.py
@@ -1,29 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-""" Module that contains the base class """
-
-import uuid
-from datetime import datetime
-
-
-class BaseModel():
-    """
-    BaseModel class:
-    """
-
-    def __init__(self, *args, **kwargs):
-        """
-        Constructor
-        """
-        if kwargs:
-            for value[key] in kwargs.items():
-                if key == "created_at":
-                    self.created_at = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                elif key == "updated_at":
-                    self.updated_at = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                else:
-                    if key != "__class__":
-=======
 """BaseModel module
 """
 import uuid
@@ -60,37 +35,11 @@ class BaseModel:
                     self.updated_at = datetime.strptime(value, format)
                 else:
                     if key != '__class__':
->>>>>>> sebastian
                         setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-<<<<<<< HEAD
-
-    def __str__(self):
-        """
-        This method is a str representation of the class
-        """
-        message = ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
-        return message
-
-    def save(self):
-        """
-        This method updates the public instance attribute updated_at
-        """
-        self.updated_at = datetime.now()
-
-    def to_dict(self):
-        """
-        This method creates a new dictionary
-        """
-        new_dictionary = dict(self.__dict__)
-        new_dictionary["created_at"] = self.created_at.isoformat()
-        new_dictionary["updated_at"] = self.updated_at.isoformat()
-        new_dictionary["__class__"] = self.__class__.__name__
-        return (new_dictionary)
-=======
             storage.new(self)
 
     def __str__(self):
@@ -131,4 +80,3 @@ class BaseModel:
         new_dict['created_at'] = self.created_at.isoformat()
         new_dict['updated_at'] = self.updated_at.isoformat()
         return (new_dict)
->>>>>>> sebastian
