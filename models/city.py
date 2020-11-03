69aa7482e0dd34c5ad37600bf6ffffa8c7f8b6e3
idelahoz14 <idelahoz14@cuc.edu.co>
2020-11-03 14:24:52 +0000

    city class

---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" Module that contains the City class """
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """ City class """
+    state_id = ""
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """ Constructor """
+        super().__init__(*args, **kwargs)
